c3b2f5b411f3cbb1abfbe4bafde10b080ca6d3ba
Rajesh <rajesh.reddy@urjanet.com>
2019-09-10 16:32:05 +0530

    getBuildUser function

--- vars/sendSlack.groovy
+++ vars/sendSlack.groovy
@@ -59,27 +59,13 @@ def getAbortUser()
 
 def call(String buildResult) {
   if ( buildResult == "STARTED" ) {
-    
-        def SCMTriggerCause
-        def UserIdCause
-        def GitHubPRCause
-        def PRCause = currentBuild.rawBuild.getCause(org.jenkinsci.plugins.github.pullrequest.GitHubPRCause)
-        def SCMCause = currentBuild.rawBuild.getCause(hudson.triggers.SCMTrigger$SCMTriggerCause)
-        def UserCause = currentBuild.rawBuild.getCause(hudson.model.Cause$UserIdCause)
 
-        if (PRCause) {
-            println PRCause.getShortDescription()
-            slackSend color: "good", message: "${env.JOB_NAME} - Build: <${env.BUILD_URL}|#${env.BUILD_NUMBER}> Started by " + PRCause.getShortDescription() + "\nChanges:\n" + "\t" + getChangeString()
-        } else if (SCMCause) {
-            println SCMCause.getShortDescription()
-            slackSend color: "good", message: "${env.JOB_NAME} - Build: <${env.BUILD_URL}|#${env.BUILD_NUMBER}> Started by " + author() + "\nChanges:\n" + "\t" + getChangeString()
-        } else if (UserCause) {
-            println UserCause.getShortDescription()
-            slackSend color: "good", message: "${env.JOB_NAME} - Build: <${env.BUILD_URL}|#${env.BUILD_NUMBER}> Started by " + UserCause.getShortDescription() + "\nChanges:\n" + "\t" + getChangeString()
-        }else {
-           println "unknown cause"
-        }
-    // slackSend color: "good", message: "${env.JOB_NAME} - Build: <${env.BUILD_URL}|#${env.BUILD_NUMBER}> Started by " + getBuildUser() + "\nChanges:\n" + "\t" + getChangeString()
+    def causes = currentBuild.rawBuild.getCauses()
+
+    println causes.dump()
+    println causes
+    
+    slackSend color: "good", message: "${env.JOB_NAME} - Build: <${env.BUILD_URL}|#${env.BUILD_NUMBER}> Started by " + author() + "\nChanges:\n" + "\t" + getChangeString()
   }
 
   //DEV Stage notification
